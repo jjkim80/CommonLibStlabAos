5d886f116bfc5eb012c71618395b990d6cf639ca
jjkim <jjkim@dkitec.com>
2022-04-28 10:12:14 +0900

    암호화 모듈 추가

--- app/src/main/java/commonlib/stlab/aos/MainActivity.java
+++ app/src/main/java/commonlib/stlab/aos/MainActivity.java
@@ -1,12 +1,18 @@
 package commonlib.stlab.aos;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.dkitec.commonlib_stalb_aos.EncryptionActivity;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(MainActivity.this, EncryptionActivity.class);
+        startActivity(intent);
     }
 }
